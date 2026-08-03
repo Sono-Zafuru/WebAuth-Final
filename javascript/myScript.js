72f949e9cf86eeb21eaae9276cd1d5f53415fd29
Solano Sayasetha <104727556+Sono-Zafuru@users.noreply.github.com>
2026-08-03 19:57:12 -0400

    Add modal functionality to myScript.js

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -2,3 +2,22 @@
     Solano Sayasetha
     myScript.js
 */
+
+/* Script for modal */
+var modal = document.getElementById("myModal");
+
+document.querySelectorAll('.galImg').forEach(function(image) {
+    var modalImg = document.getElementById("imgIn");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+});
